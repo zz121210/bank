619931ed35499ddd5d343d274e42760afca4ddbe
smy4778@naver.com <smy4778@naver.com>
2022-03-13 18:02:53 +0900

    3.add login fuction 20220313 김준성

--- routes/user/login/route.js
+++ routes/user/login/route.js
@@ -7,4 +7,19 @@ router.get("/", (req, res) => {
   res.render("../../routes/user/login/views/index.ejs")
 })
 
+router.post("/login_process", (req, res) => {
+  const {m_id, m_pw} = req.body
+
+  db.query("select * from waw_member where m_id=? and m_pw=?", [m_id, m_pw], (err, result) => {
+    if(result.length > 0) {
+      console.log("pass")
+      req.session.id = result.m_id
+      console.log(req.session.id)
+      req.session.save(() => {
+        res.redirect('/')
+      })
+    }
+  })
+})
+
 module.exports = router;
